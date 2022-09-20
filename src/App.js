d8237c4f19cb3f4683813506985462ad95998b94
Cem <cmbicakci@gmail.com>
2022-09-20 20:10:51 +0300

    added nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost'
 import Login from './pages/Login'
+import './App.css'
 
 function App() {
-    return (
+  return (
 
-      <Router>
+    <Router>
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/createpost' element={<CreatePost />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </Router>
-    );
-  }
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/createpost'>Create Post</Link>
+        <Link to='/login'>Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/createpost' element={<CreatePost />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
