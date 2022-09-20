61f938f727e9a7a5699a734932dffb64d8027200
Cem <cmbicakci@gmail.com>
2022-09-20 22:21:38 +0300

    added create post page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     signOut(auth).then(() => {
       localStorage.clear();
       setIsAuth(false);
+      //Router varken useNavigate kullanılamıyor. Ama window.location.pathmame ile yönledirme yapılabilir.
       window.location.pathname = '/login'
 
     })
@@ -26,13 +27,20 @@ function App() {
 
       <nav>
         <Link to='/'>Home</Link>
-        <Link to='/createpost'>Create Post</Link>
-        {!isAuth ? <Link to='/login'>Login</Link> : <button onClick={signUserOut}>Log Out</button>}
+        {
+          !isAuth
+            ? <Link to='/login'>Login</Link>
+            :
+            <>
+              <Link to='/createpost'>Create Post</Link>
+              <button onClick={signUserOut}>Log Out</button>
+            </>
+        }
       </nav>
 
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/createpost' element={<CreatePost />} />
+        <Route path='/createpost' element={<CreatePost isAuth={isAuth}/>} />
         <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import { db, auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-function CreatePost() {
+function CreatePost({ isAuth }) {
+
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState('');
+  const [postText, setPostText] = useState('');
+
+  //input taki girdileri firebase database aktarmak için. Collection ismi posts olacak şekilde.
+  const postCollectionRef = collection(db, 'posts')
+  const createPost = async () => {
+    await addDoc(postCollectionRef, {
+      title,
+      postText,
+      author: { name: auth.currentUser.displayName, id: auth.currentUser.uid }
+    });
+
+    navigate('/');
+  }
+
+
+  // Eğer kullanıcı girişi yoksa, direk login sayfasına yönlendir.
+  useEffect(() => {
+    if (!isAuth) {
+      navigate('/login');
+    }
+  }, [])
+
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+
+      <div className='cpContainer'>
+
+        <h1>Create A Post</h1>
+
+        <div className='inputGp'>
+          <label>Title:</label>
+          <input placeholder='Title...' onChange={(e) => setTitle(e.target.value)} />
+        </div>
+
+        <div className='inputGp'>
+          <label>Post:</label>
+          <textarea placeholder='Post...' onChange={(e) => setPostText(e.target.value)} />
+        </div>
+
+        <button onClick={createPost}>Submit Post</button>
+      </div>
+    </div>
   )
 }
 
