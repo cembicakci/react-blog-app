b32b6f276f0666ab9eec8339dbbc6cc23e5fa222
Cem <cmbicakci@gmail.com>
2022-09-21 22:10:12 +0300

    get data from db

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getDocs, collection } from 'firebase/firestore' //database deki verilei çekmek için 
+import { db } from '../firebase';
 
 function Home() {
+
+  const [postList, setPostList] = useState([]);
+  const postCollectionRef = collection(db, 'posts')
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef)
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+
+    getPosts()
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className='homePage'>
+
+    </div>
   )
 }
 
